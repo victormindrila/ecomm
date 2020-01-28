0897c6dcf31ae45e8a4c6c4612eba324d68d11ec
Victor Mindrila <victormindrila@gmail.com>
2020-01-28 19:10:03 +0200

    final project

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session');
+
+const app = express();
+const authRouter = require('./routes/admin/auth');
+const adminProductsRouter = require('./routes/admin/products');
+const productsRouter = require('./routes/products');
+const cartsRouter = require('./routes/carts');
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+	cookieSession({
+		keys: [ 'lkjflaskjflaskjfal;kj' ]
+	})
+);
+
+app.use(authRouter);
+app.use(adminProductsRouter);
+app.use(productsRouter);
+app.use(cartsRouter);
+
+app.listen(3000, () => {
+	console.log('Listening');
+});
---
+++ routes/admin/auth.js
@@ -0,0 +1,59 @@
+const express = require('express');
+
+const { handleErrors } = require('./middlewares');
+const usersRepo = require('../../repositories/users');
+const signupTemplate = require('../../views/admin/auth/signup');
+const signinTemplate = require('../../views/admin/auth/signin');
+const {
+	requireEmail,
+	requirePassword,
+	requirePasswordConfirmation,
+	requireEmailExists,
+	requireVAlidPassowordForUser
+} = require('./validators');
+
+const router = express.Router();
+router.get('/signup', (req, res) => {
+	res.send(signupTemplate({ req }));
+});
+
+router.post(
+	'/signup',
+	[ requireEmail, requirePassword, requirePasswordConfirmation ],
+	handleErrors(signupTemplate),
+	async (req, res) => {
+		const { email, password } = req.body;
+		const user = await usersRepo.create({ email, password });
+
+		req.session.userId = user.id; // store the id of that suer inside the user cookie
+
+		res.redirect('/admin/products');
+	}
+);
+
+router.get('/signout', (req, res) => {
+	req.session = null;
+	res.send('You are logged out');
+});
+
+router.get('/signin', (req, res) => {
+	res.send(signinTemplate({}));
+});
+
+router.post(
+	'/signin',
+	[ requireEmailExists, requireVAlidPassowordForUser ],
+	handleErrors(signinTemplate),
+	async (req, res) => {
+		const { email } = req.body;
+
+		const user = await usersRepo.getOneby({ email });
+
+		req.session.userId = user.id;
+		console.log(req.session.userId);
+
+		res.redirect('/admin/products');
+	}
+);
+
+module.exports = router;
